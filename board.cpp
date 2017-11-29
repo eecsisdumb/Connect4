566b42416f17564cd0af838ee14e440810677a63
adishy <33792934+adishy@users.noreply.github.com>
2017-11-29 08:08:06 -0500

    Add files via upload

--- board.cpp
+++ board.cpp
@@ -1,12 +1,103 @@
+#ifndef iostream
+#include <iostream>
+#endif
+
 #include "board.h"
 
 Board::Board() {
+	PieceType empty = Empty;
+
+	for (int i = 0; i < NUM_ROWS; i++) {
+		for (int j = 0; j < NUM_COLS; j++) {
+			data[i][j] = empty;
+		}
+	}
+
+	nextPlayer_to_move = Player1;
 }
 
 Board::Board(const string &fen) {
+	int currentRowToFill = 0;
+	int numberOfSpacesInGivenRow = 0;
+	int filledColumn = 0;
+
+	for (int i = 0; i < fen.length() - 2; i++) {
+		if (isdigit(fen[i])) {
+			numberOfSpacesInGivenRow = fen[i] - 48;
+			int j = filledColumn;
+			for (; j < numberOfSpacesInGivenRow + filledColumn && filledColumn < NUM_COLS; j++)
+				data[currentRowToFill][filledColumn] = Empty;
+			filledColumn = j;
+
+			numberOfSpacesInGivenRow = 0;
+		}
+
+		if (fen[i] == 'x') {
+			data[currentRowToFill][filledColumn] = Player1;
+			filledColumn++;
+		}
+
+
+		if (fen[i] == 'o') {
+			data[currentRowToFill][filledColumn] = Player2;
+			filledColumn++;
+		}
+
+		if (fen[i] == '/') {
+			currentRowToFill++;
+			filledColumn = 0;
+			numberOfSpacesInGivenRow = 0;
+		}
+
+	}
+
+	if (fen[fen.length() - 1] == 'x') {
+		nextPlayer_to_move = Player1;
+	}
+
+	else if (fen[fen.length() - 1] == 'o') {
+		nextPlayer_to_move = Player2;
+	}
+
+
 }
 
 void Board::printBoard_as_FENstring(ostream &os) const {
+	int spaceInBoard = 0;
+	for (int currentRow = NUM_ROWS - 1; currentRow >= 0; currentRow--) {
+		spaceInBoard = 0;
+		for (int i = 0; i < NUM_COLS; i++) {
+			if (data[currentRow][i] == Empty) {
+				spaceInBoard++;
+			}
+
+			else if (data[currentRow][i] == Player1) {
+				if (spaceInBoard > 0)
+					os << spaceInBoard;
+				spaceInBoard = 0;
+				os << "x";
+			}
+
+			else if (data[currentRow][i] == Player2) {
+				if (spaceInBoard > 0)
+					os << spaceInBoard;
+				spaceInBoard = 0;
+				os << "o";
+			}
+		}
+		if (spaceInBoard > 0 && spaceInBoard <= 7)
+			os << spaceInBoard;
+		if (currentRow > 0)
+			os << "/";
+	}
+
+	if (nextPlayer_to_move == Player1) {
+		os << " " << "x";
+	}
+
+	else if (nextPlayer_to_move == Player2) {
+		os << " " << "o";
+	}
 }
 
 PieceType Board::atLocation(int row, int col) {
@@ -37,35 +128,129 @@ void Board::prettyPrintBoard(ostream &os) const {
 }
 
 int Board::toMove() const {
-    return 1;
+	if (nextPlayer_to_move == Player1)
+		return 2;
+
+	else if (nextPlayer_to_move == Player2)
+		return 1;
+
+	return 0;
 }
 
 Result Board::makeMove(int col) {
-    return NoResult;
+	int givenRowToInsert = getFirstFreeRow(col);
+	int givenColToInsert = col;
+
+	if (!inBounds(givenRowToInsert, givenColToInsert)) {
+		return IllegalMove;
+	}
+
+	if (isWin(givenRowToInsert, givenColToInsert)) {
+		return Win;
+	}
+
+	else if (isBoardFull()) {
+		return Draw;
+	}
+
+	if (toMove() == 1) {
+			data[givenRowToInsert][givenColToInsert] = Player1;
+	}
+
+	else if (toMove() == 2) {
+		data[givenRowToInsert][givenColToInsert] = Player2;
+	}
+
+	updateToMove();
+
+	return NoResult;
 }
 
 int Board::getFirstFreeRow(int col) const {
-    return NUM_ROWS;
+	for (int i = 0; i < NUM_ROWS; i++)
+		if (data[i][col] == Empty)
+			return i;
+
+	return NUM_ROWS;
 }
 
 PieceType Board::updateToMove() {
-    return Player1;
+
+	if (nextPlayer_to_move == Player1) {
+		nextPlayer_to_move = Player2;
+		return Player1;
+	}
+
+	else if (nextPlayer_to_move == Player2) {
+		nextPlayer_to_move = Player1;
+		return Player2;
+	}
 }
 
 bool Board::isBoardFull() const {
-    return false;
+	for (int i = 0; i < NUM_ROWS; i++) {
+		for (int j = 0; j < NUM_COLS; j++) {
+			if (data[i][j] == Empty)
+				return false;
+		}
+	}
+
+	return true;
 }
 
 bool Board::inBounds(int row, int col) const {
-    return false;
+	if (row < 0)
+		return false;
+
+	if (row >= NUM_ROWS)
+		return false;
+
+	if (col < 0)
+		return false;
+
+	if (col >= NUM_COLS)
+		return false;
+
+	return true;
 }
 
 int Board::piecesInDirection(int row, int col, int dRow, int dCol) const {
-    return 0;
+	PieceType somePlayer = data[row][col];
+	int matchingPieces = 0;
+
+	row = row + dRow;
+	col = col + dCol;
+
+	while (data[row][col] == somePlayer && row >= 0 && col >= 0 && row < NUM_ROWS && col < NUM_COLS) {
+		matchingPieces++;
+		row = row + dRow;
+		col = col + dCol;
+	}
+
+	return matchingPieces;
 }
 
 bool Board::isWin(int row, int col) const {
-    return false;
+	if (data[row][col] == Empty) {
+		return false;
+	}
+
+	int fourPiecesConsecutiveInRowDown = 1 + piecesInDirection(row, col, 1, 0);
+	int fourPiecesConsecutiveInRowUp = 1 + piecesInDirection(row, col, -1, 0);
+	int fourPiecesConsecutiveInColRight = 1 + piecesInDirection(row, col, 0, 1);
+	int fourPiecesConsecutiveInColLeft = 1 + piecesInDirection(row, col, 0, -1);
+	int fourPiecesConsecutiveInLeftDiagonalDown = 1 + piecesInDirection(row, col, 1, -1);
+	int fourPiecesConsecutiveInRightDiagonalDown = 1 + piecesInDirection(row, col, 1, 1);
+	int fourPiecesConsecutiveInLeftDiagonalUp = 1 + piecesInDirection(row, col, -1, -1);
+	int fourPiecesConsecutiveInRightDiagonalUp = 1 + piecesInDirection(row, col, -1, 1);
+
+	if (fourPiecesConsecutiveInColLeft == 4 || fourPiecesConsecutiveInColRight == 4 || fourPiecesConsecutiveInRowDown == 4 || fourPiecesConsecutiveInRowUp == 4 || fourPiecesConsecutiveInLeftDiagonalDown == 4) {
+		return true;
+	}
+
+	if (fourPiecesConsecutiveInLeftDiagonalDown == 4 || fourPiecesConsecutiveInLeftDiagonalUp == 4 || fourPiecesConsecutiveInRightDiagonalDown == 4 || fourPiecesConsecutiveInRightDiagonalUp == 4) {
+		return true;
+	}
+	
+	return false;
 }
-
-
--- connect4.cpp
+++ connect4.cpp
@@ -7,23 +7,122 @@
 using namespace std;
 
 void terminalPlay(){
-    cout << "\nEnter: the column to move, q to quit, s to save" << endl;
-    cout <<  " enter your move: ";
-    cout << "Thanks for playing!"<< endl;
-    cout << "ILLEGAL MOVE: Try again" << endl;
-    cout << "Congratulations ";
-    cout << "! You won!" << endl;
-    cout << "Draw!" << endl;
+
+	string playerAName = "";
+	string playerBName = "";
+	string currentPlayer = "";
+	Board someGame = getBoard(playerAName, playerBName);
+	Result moveResult = NoResult;
+	char playerInputFromKeyboard = ' ';
+	int currentColumnSelected = 0;
+
+	currentPlayer = playerAName;
+
+	while (true) {
+
+		someGame.prettyPrintBoard(cout);
+		
+		if (moveResult == Win) {
+			cout << "Congratulations ";
+			cout << "! You won!" << endl;
+			return;
+		}
+
+		else if (moveResult == IllegalMove) {
+			cout << "ILLEGAL MOVE: Try again" << endl;
+		}
+
+		else if (moveResult == Draw) {
+			cout << "Draw!" << endl;
+			return;
+		}
+
+		cout << "\nEnter: the column to move, q to quit, s to save" << endl;
+		cout << " enter your move: ";
+
+		cin >> playerInputFromKeyboard;
+
+		if (tolower(playerInputFromKeyboard) == 'q') {
+			break;
+		}
+
+		else if (tolower(playerInputFromKeyboard) == 's') {
+			saveBoard(playerAName, playerBName, someGame);
+		}
+		
+		else if (isdigit(playerInputFromKeyboard)) {
+
+			currentColumnSelected = playerInputFromKeyboard - 48;
+		
+			moveResult = someGame.makeMove(currentColumnSelected - 1);
+
+		}
+	}
+
+	cout << "Thanks for playing!" << endl;
 }
 
 Board getBoard(string &p1, string &p2) {
-    cout << "Enter L to load a game, or P to play: " << flush;
-    cout << "Player 1, enter your name: " << flush;
-    cout << endl << "Player 2, enter your name: " << flush;
-    cout << "Enter the filename: " << flush;
-    return Board();
+	char modeIndicatorCharacter = ' ';
+	string givenUserInput = "";
+	string gameStateFromFile = "";
+	Board someBoard;
+	ifstream gameInFile;
+
+	cout << "Enter L to load a game, or P to play: " << flush;
+	cin >> modeIndicatorCharacter;
+
+	if (toupper(modeIndicatorCharacter) == 'P') {
+
+		cout << "Player 1, enter your name: " << flush;
+		
+		cin >> givenUserInput;
+
+		p1 = givenUserInput;
+
+		cout << endl << "Player 2, enter your name: " << flush;
+
+		cin >> givenUserInput;
+
+		p2 = givenUserInput;
+
+	}
+
+	else if (toupper(modeIndicatorCharacter) == 'L') {
+		cout << "Enter the filename: " << flush;
+		
+		cin >> givenUserInput;
+
+		gameInFile.open(givenUserInput);
+
+		getline(gameInFile, p1);
+
+		getline(gameInFile, p2);
+
+		getline(gameInFile, gameStateFromFile);
+
+		someBoard = Board(gameStateFromFile);
+
+	}
+   
+	return someBoard;
 }
 
 void saveBoard(string p1, string p2, const Board &board) {
-    cout << "Enter the filename: " << flush;
+	string providedSaveFilename = "";
+	ofstream gameToFile;
+
+	cout << "Enter the filename: " << flush;
+
+	cin >> providedSaveFilename;
+
+	gameToFile.open(providedSaveFilename);
+
+	gameToFile << p1 << endl;
+
+	gameToFile << p2 << endl;
+
+	board.printBoard_as_FENstring(gameToFile);
+	
+	gameToFile.close();
 }
